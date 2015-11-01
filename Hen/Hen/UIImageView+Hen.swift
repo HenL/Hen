d5ec13109e9b3930ab24bfe79a48950bf6780adb
Hen Levy <www.hen@gmail.com>
2015-11-01 18:05:31 +0200

    imageview id handle

--- Hen/Hen/UIImageView+Hen.swift
+++ Hen/Hen/UIImageView+Hen.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
+// Declare a global var to produce a unique address as the assoc object handle
+var AssociatedObjectHandle: UInt8 = 0
+
 public extension UIImageView {
+    
+    var id: String {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedObjectHandle) as? String ?? ""
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedObjectHandle, newValue, objc_AssociationPolicy(rawValue: 1)!)
+        }
+    }
     
     public func setImageFromStrURL(strUrl: String?, placeholder: UIImage? = nil) {
 
-        Hen.sharedInstance.fetchImage((self as! HenImageView), strUrl: strUrl, placeholder: placeholder)
+        Hen.sharedInstance.fetchImage(self, strUrl: strUrl, placeholder: placeholder)
     }
-}
-
-class HenImageView: UIImageView {
-    var id: String?
 }
